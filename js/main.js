632394375e7a3e2412d7cbb3cbf4d494640131ea
gena granatov <102759762+granatov@users.noreply.github.com>
2022-06-01 01:21:13 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".contact_slider").slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+  });
+});
