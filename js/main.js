72c526be01db7c9d2a1b787c12cac914807e7ebc
gena granatov <102759762+granatov@users.noreply.github.com>
2022-06-02 18:35:30 +0500

    add response

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(function () {
+  $(".main_slider").slick({
+    arrows: true,
+    dots: true,
+    prevArrow: '<img class="slick-prev" src="template/prev_arr.svg" alt="">',
+    nextArrow: '<img class="slick-next" src="template/next_arr.svg" alt="">',
+    swipe: false,
+    speed: 1500,
+    fade: true,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 568,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+
   $(".contact_slider").slick({
     infinite: true,
     slidesToShow: 6,
@@ -6,5 +25,19 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 1000,
     arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
+  });
+
+  $(".menu_brg").click(function () {
+    $(".menu_brg,.header__menu-list").toggleClass("active");
   });
 });
